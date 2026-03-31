f50ad466a100bda1e7f124cbcddcdfd781329ac9
Rahul750-git <varmarahul08501@gmail.com>
2026-03-31 18:46:51 +0530

    upload files

---
+++ 01basicreact/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 style={{ color: "red", backgroundColor: "lightgray" }}>
+      CodeWithRahul || with react
+    </h1>
+  );
+}
+
+export default App;
---
+++ 02component/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <h1 style={{ color: "red", backgroundColor: "lightgrey" }}>Hello React </h1>
+  );
+}
+export function Comp() {
+  return (
+    <>
+      <h1 style={{ color: "red", backgroundColor: "lightgrey" }}>
+        CodeWithRahul || with react
+      </h1>
+      <p
+        style={{ color: "red", backgroundColor: "lightgrey", fontSize: "40px" }}
+      >
+        This is a component
+      </p>
+    </>
+  );
+}
+
+export default App;
