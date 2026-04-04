47d06ec58a1f83329ad3b0d8521094a560f9be06
Rahul750-git <varmarahul08501@gmail.com>
2026-04-04 18:47:30 +0530

    upload file

---
+++ 19-cards-project/src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./index.css";
+import { Bookmark } from "lucide-react";
+const App = () => {
+  return (
+    <div className="parent">
+      <div className="card">
+        <div className="top">
+          <img
+            src="https://images.seeklogo.com/logo-png/29/2/amazon-icon-logo-png_seeklogo-297884.png"
+            alt="amazon logo "
+          />
+          <button>
+            Save
+            <Bookmark />
+          </button>
+        </div>
+        <div className="center">
+          <h3>
+            Amazon
+            <span> 5 days ago</span>
+          </h3>
+          <h2>Senior UI/UX Designer</h2>
+          <div>
+            <h4>Part Time</h4>
+            <h4>Senior Level</h4>
+          </div>
+        </div>
+        <div className="bottom">
+          <div>
+            <div>
+              <h3>$120/hr</h3>
+              <p>Mumbai ,India</p>
+            </div>
+            <button>Apply Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
