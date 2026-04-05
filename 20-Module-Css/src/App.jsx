9afe2ec03d3f2be5da61dbdda84ad6c16758470c
Rahul750-git <varmarahul08501@gmail.com>
2026-04-05 18:57:30 +0530

    upload file

---
+++ 20-Module-Css/src/App.jsx
@@ -0,0 +1,13 @@
+import Button from "./component/button/Button."
+
+function App() {
+  
+
+  return (
+   <div>
+    <Button />
+   </div>
+  )
+}
+
+export default App
---
+++ 20-Module-Css/src/component/button/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './button.css'
+const Button = () => {
+  return (
+    <div>
+        <button className="btn">Click me</button>
+    </div>
+  )
+}
+
+export default Button
