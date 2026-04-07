c2cecaf3dfbcedff0636bd3a2efdc4c32c4da97e
Rahul750-git <varmarahul08501@gmail.com>
2026-04-07 18:02:21 +0530

    upload file

---
+++ 23-Functions/src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+  
+function btnclicked(){
+console.log("Hello ,button is Clicked");
+
+
+}
+  return (
+<div >
+<h1>Hello ,Guys</h1>
+
+<button onClick={btnclicked}>Click me</button>
+</div>
+  )
+}
+
+export default App
